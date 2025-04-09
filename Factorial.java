b98b1ea2eb012b920be609fdd9a04e977b2ffc52
annumallesh28 <annapur28@gmail.com>
2025-04-09 10:05:17 +0530

    hey

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+package Javaprograms;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        long factorial = 1; // Use 'long' to handle larger factorial values
+        
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        
+        System.out.println("Factorial of " + number + " is: " + factorial);
+        
+        scanner.close();
+    }
+}
+
