0df365ffe3f8e2bea3ca159a983e1cf86b1e1973
annumallesh28 <annapur28@gmail.com>
2025-04-09 09:46:44 +0530

    save java

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+package Javaprograms;
+class Datatypes{
+    public static void main (String[]args){
+        char a ='b';
+        int i = 80;
+        byte b =2;
+        short s =56;
+        double d =4.33;
+        float f =1.3f;
+        System.out.println("char:"+a);
+        System.out.println("int:"+i);
+        System.out.println("byte:"+b);
+        System.out.println("double:"+d);
+    }
+}
